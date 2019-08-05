b6557f02a9c91ec47d1949309405e74527bdca9d
THIZY Kevin <kevin.thizy@outlook.fr>
2019-08-05 13:31:37 +0200

    ✨() Export claim topics by name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import ClaimTopics from './claim_topics.json';
+import _ from 'lodash';
+
+import ClaimTopicsById from './claim_topics.json';
+
+const ClaimTopicsByName = _.fromPairs(Object.entries(ClaimTopicsById).map(([id, claimTopic]: [string, any]) => [claimTopic.name, claimTopic]));
 
 export default {
-  ClaimTopics,
+  ClaimTopicsById,
+  ClaimTopicsByName,
+}
+
+export {
+  ClaimTopicsById,
+  ClaimTopicsByName,
 };
